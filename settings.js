208682e3bf937760c0eaa51e5bd185d1f12ba3a8
David Merrick <david.merrick@jivesoftware.com>
2017-03-30 08:14:57 -0700

    Updated default ports

--- settings.js
+++ settings.js
@@ -15,8 +15,8 @@ function merge(target, source) {
 }
 
 var settings = {
-  port: 5005,
-  securePort: 5006,
+  port: 80,
+  securePort: 443,
   cacheDir: path.resolve(__dirname, 'cache'),
   webroot: path.resolve(__dirname, 'static'),
   presetDir: path.resolve(__dirname, 'presets'),
